9d12c05a0c970496252d4af8c65d29f7f3826606
Turan98 <48137040+Turan98@users.noreply.github.com>
2023-02-25 12:18:16 +0400

    test 21

---
+++ array_inverse/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] array = new int [] {4, 6, 0, -2, 34, 8};
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + "\t");
+}
+Console.WriteLine();
+
+
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    
+    Console.Write(array[i] + "\t");
+   
+}
+
+
+
+
+
